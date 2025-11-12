4f133dc8a0b00e6f9778efffc77d55d871d0b2e9
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-12 10:27:54 +0530

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,15 @@
+number=int(input("enter the number"))
+for a in range(1,11):
+  print(f"{number} x {a} ={number*a} ")
+enter the number9
+9 x 1 =9 
+9 x 2 =18 
+9 x 3 =27 
+9 x 4 =36 
+9 x 5 =45 
+9 x 6 =54 
+9 x 7 =63 
+9 x 8 =72 
+9 x 9 =81 
+9 x 10 =90 
+
