f007cf36e76f590b9df94e3c255ed15d565020b1
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-12 11:58:35 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,19 @@
+number=int(input("enter the number"))
+factorial=1
+for a in range(1,number+1):
+  factorial*=a
+  print(factorial)
+enter the number10
+1
+2
+6
+24
+120
+720
+5040
+40320
+362880
+3628800
+
+
+
