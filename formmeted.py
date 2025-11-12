68100e7fd924bf07334699e3c19449cfb8f260dc
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-12 10:26:11 +0530

    Create formmeted.py

---
+++ formmeted.py
@@ -0,0 +1,7 @@
+name="prarthana"
+age=19
+college="siet"
+department="AIandML"
+print(f"my name is:{name} my age : {age} my college is : {college} department: {department}")
+my name is:prarthana my age : 19 my college is : siet department: AIandML
+
